f9096a12c3479da3eab7852ea0e377dc5888d020
kimly.888 <kimly.jet@gmail.com>
2023-02-06 22:40:31 +0900

    Feat: Add handler for theme change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,23 @@
+import { useStateContext } from "@/context/StateContext";
+
 const Navbar = () => {
+  const { isActive, setIsActive, isDarkMode, setIsDarkMode } =
+    useStateContext();
+
+  const handleMobileMenu = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  const handleTheme = (e) => {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark"); //add this
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light"); //add this
+    }
+  };
+
   return (
     <div>
       {/* <!-- Navbar --> */}
@@ -11,19 +30,23 @@ const Navbar = () => {
             </a>
           </h1>
           {/* <!-- Navbar links --> */}
-          <ul className="nav-menu">
+          <ul className={`nav-menu ${isActive && "active"}`}>
             <li>
-              <a className="nav-link" href="#projects">
+              <a
+                className="nav-link"
+                href="#projects"
+                onClick={handleMobileMenu}
+              >
                 PROJECTS
               </a>
             </li>
             <li>
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#" onClick={handleMobileMenu}>
                 CONTACT
               </a>
             </li>
             <li>
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#" onClick={handleMobileMenu}>
                 BlOG
               </a>
             </li>
@@ -31,6 +54,7 @@ const Navbar = () => {
               <a
                 className="nav-link btn btn-primary"
                 href="https://github.com/CommunityPro/portfolio-html"
+                onClick={handleMobileMenu}
               >
                 RESUME <i className="fas fa-arrow-right"></i>
               </a>
@@ -38,7 +62,11 @@ const Navbar = () => {
 
             {/* <!-- Toggle switch --> */}
             <div className="theme-switch">
-              <input type="checkbox" id="switch" />
+              <input
+                type="checkbox"
+                id="switch"
+                onClick={(e) => handleTheme(e)}
+              />
               <label className="toggle-icons" htmlFor="switch">
                 <img className="moon" src="/moon.svg" />
                 <img className="sun" src="/sun.svg" />
@@ -47,7 +75,10 @@ const Navbar = () => {
           </ul>
 
           {/* <!-- Hamburger menu --> */}
-          <div className="hamburger">
+          <div
+            className={`hamburger ${isActive && "active"}`}
+            onClick={handleMobileMenu}
+          >
             <span className="bar"></span>
             <span className="bar"></span>
             <span className="bar"></span>
@@ -57,4 +88,5 @@ const Navbar = () => {
     </div>
   );
 };
+
 export default Navbar;
