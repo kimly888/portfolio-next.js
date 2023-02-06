dd7ad66ef8ab310102fd163cef217d6e77e372ec
kimly.888 <kimly.jet@gmail.com>
2023-02-07 00:08:11 +0900

    Feat: Use next/font for font optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,24 @@
+import { Raleway } from "@next/font/google";
 import { Layout } from "@/components";
 import { StateContext } from "@/context/StateContext";
 
 import "@/styles/globals.css";
 import "@/styles/utilities.css";
 
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <StateContext>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
+    <div className={raleway.className}>
+      <StateContext>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StateContext>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-// import { Inter } from "@next/font/google";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   return (
